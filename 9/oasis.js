42b357b428de0a24b63e0f9ff546f167da493353
ThibWill <thibault.willer@gmail.com>
2024-02-25 21:47:14 +0100

    Day 9 step 1

---
+++ 9/oasis.js
@@ -0,0 +1,47 @@
+import { readFile } from 'node:fs/promises';
+import { resolve } from 'node:path';
+
+async function loadDocument() {
+  try {
+    const filePath = resolve('./oasis.txt');
+    const contents = await readFile(filePath, { encoding: 'utf8' })
+    return contents.trim()
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+const test = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+const parser = (doc) => {
+  return doc.split('\n').map(line => line.split(' ').map(Number))
+}
+
+const generateNextSequence = (sequence) => {
+  const sequenceDifferences = []
+  for (let i = 0; i < sequence.length - 1; i++) {
+    sequenceDifferences[i] = sequence[i + 1] - sequence[i];
+  }
+  return sequenceDifferences;
+}
+
+const processSequence = (sequence) => {
+  const decompositions = [sequence];
+  while (decompositions.at(-1).some(value => value !== 0)) {
+    decompositions.push(generateNextSequence(decompositions.at(-1)));
+  }
+
+  for (let i = decompositions.length - 1; i >= 0; i--) {
+    const decomposition = decompositions[i];
+    decomposition[decomposition.length] = decompositions[i + 1] ? decompositions[i + 1].at(-1) + decompositions[i].at(-1) : decompositions[i].at(-1);
+  }
+  
+  return decompositions[0].at(-1);
+}
+
+const document = await loadDocument();
+const sequences = parser(document);
+
+console.log(sequences.reduce((acc, sequence) => acc + processSequence(sequence), 0));
