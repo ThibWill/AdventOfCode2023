a0ed781a46f8e072e9feeb00fa4f9c71cc4c7d73
ThibWill <thibault.willer@gmail.com>
2024-02-25 21:57:38 +0100

    Day 9 step 2

--- 9/oasis.js
+++ 9/oasis.js
@@ -16,7 +16,7 @@ const test = `0 3 6 9 12 15
 10 13 16 21 30 45`;
 
 const parser = (doc) => {
-  return doc.split('\n').map(line => line.split(' ').map(Number))
+  return doc.split('\n').map(line => line.split(' ').map(Number));
 }
 
 const generateNextSequence = (sequence) => {
@@ -33,12 +33,14 @@ const processSequence = (sequence) => {
     decompositions.push(generateNextSequence(decompositions.at(-1)));
   }
 
-  for (let i = decompositions.length - 1; i >= 0; i--) {
-    const decomposition = decompositions[i];
-    decomposition[decomposition.length] = decompositions[i + 1] ? decompositions[i + 1].at(-1) + decompositions[i].at(-1) : decompositions[i].at(-1);
-  }
+  const reversedDecompositions = decompositions.map(decomposition => decomposition.toReversed());
+
+  for (let i = reversedDecompositions.length - 1; i >= 0; i--) {
+     const decomposition = reversedDecompositions[i];
+     decomposition[decomposition.length] = reversedDecompositions[i + 1] ? reversedDecompositions[i].at(-1) - reversedDecompositions[i + 1].at(-1) : reversedDecompositions[i].at(-1);
+   }
   
-  return decompositions[0].at(-1);
+  return reversedDecompositions[0].at(-1);
 }
 
 const document = await loadDocument();
